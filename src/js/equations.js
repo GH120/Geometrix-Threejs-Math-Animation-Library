a311c87d55e78ce1fc99ff462ef18b890a4528f6
GH120 <felipevieiraduarte195@gmail.com>
2023-08-25 10:46:39 -0300

    melhorada UI do menu de equações

--- src/js/equations.js
+++ src/js/equations.js
@@ -1,3 +1,20 @@
+//Valores mapeando para objetos mesh
+//Equações veem se alguma de suas variáveis aceitam esses valores
+//Valores podem ser definidos, ou em função de outra variável
+//Variáveis são subdivididas em instanciadas e dadas
+//As instanciadas aceitam valores, mas tem seus requerimentos para funcionarem
+//Por exemplo, o pitágoras requer que as duas primeiras sejam catetos e o triângulo seja retângulo
+//As dadas são criadas pelo usuário ou impostas pelo problema, essas podem ser obtidas resolvendo as equações
+//Um exemplo prático: considere um triângulo retângulo com lados x, x-1 e x-2. 
+//Esses são três valores em função de uma variável
+//Conseguimos instânciar a equação de pitágoras, resultando em uma nova equação
+//(x-1)² + (x-2)² = x²
+//Resolvendo essa equação chegamos em:
+// x² -6x + 5 = 0
+//Onde podemos instânciar baskhara para solucionar:
+// x = 3 +- 2 => x = { 1 ; 5}
+//Como todos os lados são positivos, então x = 5;
+
 class Pythagoras {
 
     constructor(){
@@ -9,6 +26,11 @@ class Pythagoras {
             'b': ['b', handleLetterClick],
             'c': ['c', handleLetterClick]
         }
+        this.updateEquationContent()
+        this.updateEquationContent()
+        this.updateEquationContent()
+        this.updateEquationContent()
+        this.updateEquationContent()
 
     }
 
@@ -16,6 +38,10 @@ class Pythagoras {
 
         const equationContent = document.createElement("div");
         equationContent.id = "equationContent";
+
+        equationContent.style.fontFamily = "Courier New, monospace";
+        equationContent.style.fontSize = "25px";
+        equationContent.style.fontWeight ="italic";
       
         const equationWindow = document.getElementById("equationWindow");
       
@@ -51,7 +77,7 @@ class Pythagoras {
 
 document.addEventListener("DOMContentLoaded", function() {
     const openButton = document.getElementById("openEquationWindow");
-    const closeButton = document.getElementById("closeEquationWindow");
+    const closeButton = document.getElementById("closeButton");
     const equationWindow = document.getElementById("equationWindow");
 
     openButton.addEventListener("click", function() {

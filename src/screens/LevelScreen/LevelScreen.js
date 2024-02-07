d0c40b95098fb9579be6f36d085a2339c2203ed8
Thomaz <thomaz2pinheiro@gmail.com>
2024-02-07 14:10:41 -0300

    erro no whiteboard

--- src/screens/LevelScreen/LevelScreen.js
+++ src/screens/LevelScreen/LevelScreen.js
@@ -1,14 +1,73 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { useParams } from 'react-router-dom'
+import { Fase1 } from '../../js/fases/fase1';
+import { Fase2 } from '../../js/fases/fase2';
+import { Fase3 } from '../../js/fases/fase3';
+import { Fase4 } from '../../js/fases/fase4';
+import { Fase5 } from '../../js/fases/fase5';
+import { Fase6 } from '../../js/fases/fase6';
+import { Fase7 } from '../../js/fases/fase7';
+import { Whiteboard } from '../../js/cards/whiteboard';
+import Navbar from '../../components/Navbar/Navbar';
+
+const fases = {
+  '1' : Fase1,
+  '2' : Fase2,
+  '3' : Fase3,
+  '4' : Fase4,
+  '5' : Fase5,
+  '6' : Fase6,
+  '7' : Fase7,
+}
 
 const LevelScreen = () => {
 
-    const { id } = useParams();
-  
+  const { id } = useParams();
+  const refContainer = useRef(null);
+
+  // ====== INICIANDO FASE ========
+  useEffect(() => {
+    const fase = new fases[id];
+    const whiteboard = new Whiteboard();
+
+    // use ref as a mount point of the Three.js scene instead of the document.body
+    refContainer.current && refContainer.current.appendChild(fase.renderer.domElement);
+    refContainer.current && refContainer.current.appendChild(fase.labelRenderer.domElement)
+
+    fase.start(); //Começa o loop de animações
+
+    whiteboard.start();
+  }, []);
+
+  // ====== HANDLE DE CARTAS ========
+  const handleDragOver = (e) => {
+    e.preventDefault();
+  };
+
+  const handleDrop = (e) => {
+    e.preventDefault();
+    const data = e.dataTransfer.getData('text/plain');
+
+    if (data) {
+      const carta = document.querySelector(`[data-draggable="true"]`);
+
+      console.log(carta);
+
+      if (carta) {
+        carta.style.left = e.clientX + 'px';
+        carta.style.top = e.clientY + 'px';
+      }
+    }
+  };
 
   return (
-    <div>
-        <h1>Fase {id}</h1>
+    <div
+      className="App"
+      onDragOver={handleDragOver}
+      onDrop={handleDrop}
+      >
+      {/* <Navbar settings={settings}/> */}
+      <div className='container de tudo' ref={refContainer}></div>
     </div>
   )
 }
